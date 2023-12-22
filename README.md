680045d8ef62bc8202e4c06b9c0acaecd3f677cb
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-12-22 23:01:07 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple selctore of css that arrived
+
+when a element id arrived, will handle this element with js.
---
+++ index.js
@@ -0,0 +1,6 @@
+document.arrive(".jp-WindowedPanel-outer", function() {
+    // 'this' refers to the newly created element
+    this.style.paddingLeft = "300px";
+    this.style.paddingRight = "300px";
+});
+
