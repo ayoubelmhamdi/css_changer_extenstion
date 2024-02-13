fecae7afec7a3701b82257a86463e197286e9f78
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2024-02-13 18:40:19 +0100

    (index.js) add selected hosts in js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-document.arrive(".jp-WindowedPanel-outer", function() {
-    // 'this' refers to the newly created element
-    this.style.paddingLeft = "300px";
-    this.style.paddingRight = "300px";
-});
-
+if (window.location.hostname === "localhost") {
+    var doc = document.arrive(".jp-WindowedPanel-outer", function() {
+        // 'this' refers to the newly created element
+        this.style.paddingLeft = "7px";
+        this.style.paddingRight = "7px";
+    });
+}
