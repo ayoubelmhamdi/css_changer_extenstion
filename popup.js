1d105e30b2e65ce74e29ee5f49c1608327161d94
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2024-02-14 19:08:43 +0100

    add acripting permistion to popus to launch js on active tab

--- popup.js
+++ popup.js
@@ -4,8 +4,38 @@
 // });
 
 
-function functionForButton1() {
-    alert('Button 1 clicked!');
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('button1').addEventListener('click', function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.scripting.executeScript({
+                target: {tabId: tabs[0].id},
+                function: bingRemoveBg
+            });
+        });
+    });
+});
+
+function bingRemoveBg() {
+    alert('BingChat website');
+    var mainDiv = document.querySelector("#b_sydConvCont > cib-serp");
+    if (mainDiv) {
+        var cibConversationMain = mainDiv.shadowRoot.querySelector("#cib-conversation-main");
+        if (cibConversationMain) {
+            var civChatTurn = cibConversationMain.shadowRoot.querySelector("#cib-chat-main").querySelectorAll('cib-chat-turn');
+            civChatTurn.forEach(function(chatTurn) {
+                var cibMessage = chatTurn.shadowRoot.querySelector('cib-message-group.response-message-group').shadowRoot.querySelectorAll('cib-message');
+                cibMessage.forEach(function(message) {
+                    message.setAttribute("output-background", "bing-result");
+                });
+            });
+        }
+        else {
+            console.error("cibConversationMain not found");
+        }
+    }
+    else {
+        console.error("mainDiv not found");
+    }  
 }
 
 function functionForButton2() {
@@ -18,7 +48,7 @@ function functionForButton3() {
 
 // Add event listeners to your buttons when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('button1').addEventListener('click', functionForButton1);
+    // document.getElementById('button1').addEventListener('click', bingRemoveBg);
     document.getElementById('button2').addEventListener('click', functionForButton2);
     document.getElementById('button3').addEventListener('click', functionForButton3);
 });
